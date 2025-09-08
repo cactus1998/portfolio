9e1bdaf1561a08b1583fe01a2d33000af7603e33
cactus1998 <s770880qq@gmail.com>
2025-09-08 11:09:45 +0800

    新增：購物車後台卡片

--- src/data/sites.js
+++ src/data/sites.js
@@ -10,6 +10,18 @@ export const sites = [
     link: "https://yoake1.tw/fishing-reel/"
   },
   {
+    title: "樂咖大物輪 購物車",
+    description: "線上購物車系統 有後端",
+    image: logo,
+    link: "https://yoake1.tw/fishing-shop/"
+  },
+  {
+    title: "樂咖大物輪 購物車後台",
+    description: "線上購物車系統後台 須第三方登入",
+    image: logo,
+    link: "https://yoake1.tw/fishing-shop-backstage/"
+  },
+  {
     title: "react-game-tic-tac-toe",
     description: "React課程上的OOXX小遊戲",
     image: reactGame01,
@@ -20,11 +32,5 @@ export const sites = [
     description: "遊藝新境鐵丸石藝形象網站",
     image: stone,
     link: "https://yoake1.tw/yoyi/"
-  },
-  {
-    title: "樂咖大物輪 購物車",
-    description: "線上購物車系統 有後端",
-    image: logo,
-    link: "https://yoake1.tw/fishing-shop/"
   },
 ]
